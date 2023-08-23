bb86c6fad770688448e4ec6707aafde429d48264
Samcaleb <samcalebjames2017@gmail.com>
2023-08-23 13:44:50 +0530

    localhost update

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize('postgres','postgres','1234',{
-    host:'localhost',
+    host:'127.0.0.1',
     dialect:'postgres',
     logging:false,
 });
