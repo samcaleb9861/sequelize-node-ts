4fbeb911b47f095eae201029138196853e2727e0
Samcaleb <samcalebjames2017@gmail.com>
2023-08-23 13:50:55 +0530

    pool update

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,6 +4,11 @@ const sequelize = new Sequelize('postgres','postgres','1234',{
     host:'127.0.0.1',
     dialect:'postgres',
     logging:false,
+    pool: {
+        max: 9,
+        min: 0,
+        idle: 10000
+      }
 });
 
 export default sequelize;
