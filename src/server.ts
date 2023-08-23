1ac883eb3e53749efdf59413a2f11bafcec8c699
Samcaleb <samcalebjames2017@gmail.com>
2023-08-23 14:02:21 +0530

    error catch

--- src/server.ts
+++ src/server.ts
@@ -10,17 +10,19 @@ const app = express();
 
 app.use(bodyParser.json())
 
-app.use('/check',async(req:express.Request,res:express.Response)=>{
+app.use('/check', async (req: express.Request, res: express.Response) => {
     console.log("Hi there, App is running...")
-    res.json({message:"hi there, app is running"})
+    res.json({ message: "hi there, app is running" })
 })
-app.use('/users',userRoutes);
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
-sequelize.sync().then(()=>{
-    app.listen(PORT, ()=>{
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
         console.log(`Server Listening on port ${PORT}`);
     });
+}).catch((error) => {
+    console.error('Database connection error:', error);
 });
 
 
